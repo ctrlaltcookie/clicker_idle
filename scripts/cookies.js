a53bcc1b4cc2e56f49c5e764f6cfdc490a9fe895
cookie :3 <ctrlaltcookie.official@gmail.com>
2024-09-05 14:21:57 +0100

    adding an id to game

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", () => {
-  let fullOfCookies = document.cookie.split(";").find((row) => row.startsWith("acceptedCookies"))
+  let fullOfCookies = document.cookie.split(";").find((row) => row.startsWith("acceptedCookies"));
   console.log("this happened")
   if(fullOfCookies) {
-    console.log("no more cookies")
+    console.log("no more cookies");
     let popup = document.getElementById("cookie-popup");
     popup.remove();
   }
-})
+});
 
 const saveCookie = () => {
-  document.cookie = "acceptedCookies=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure"
+  document.cookie = "acceptedCookies=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure";
   let popup = document.getElementById("cookie-popup");
   popup.remove();
 };
