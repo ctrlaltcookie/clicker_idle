16155a38827315e70a99e53cadca337e9ba114b6
cookie :3 <ctrlaltcookie.official@gmail.com>
2024-09-05 14:25:47 +0100

    injecting the image

--- scripts/game.js
+++ scripts/game.js
@@ -3,5 +3,7 @@ window.addEventListener("load", () => {
 });
 
 const runGame = () => {
-  
+  let gameDiv = document.getElementById("game");
+  gameDiv.innerHTML = `<img src="images/a.png" alt="" id="a"></img>`;
+  console.log("yoooo we doing it");
 }
