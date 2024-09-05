4af098be7414665158fbe082b572f09c1c3ccc7b
cookie :3 <ctrlaltcookie.official@gmail.com>
2024-09-05 14:31:59 +0100

    game script files

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,16 @@
-window.addEventListener("load", () => {
-  runGame();
-});
+const letterFactory = () => {
+  return `<img src="images/a.png" alt="" id="a"></img>`
+}
 
 const runGame = () => {
   let gameDiv = document.getElementById("game");
-  gameDiv.innerHTML = `<img src="images/a.png" alt="" id="a"></img>`;
-  console.log("yoooo we doing it");
+  gameDiv.innerHTML = letterFactory();
 }
+
+window.addEventListener("load", () => {
+  runGame();
+
+  document.body.addEventListener("click", () => {
+    window.alert("you clicked SOMEWHERE on the screen you little gremlin you");
+  });
+});
